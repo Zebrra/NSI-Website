29e3822662183082a1535ef50460f0587409c9fd
Zebrra <78254940+Zebrra@users.noreply.github.com>
2022-03-01 18:50:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+var x = document.getElementById("my_navbar-purple");
+if (x.className === "navbar") {
+	x.className += " responsive";
+} else {
+	x.className = "navbar";
+}
+}
